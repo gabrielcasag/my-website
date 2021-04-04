d91ab1e7c4ed43e0a8c2db080e032936655ba7df
gabrielcasag <gaspericasag@gmail.com>
2021-04-03 22:57:22 -0300

    project startup, typescript, dependencies

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Avatar from "../components/Avatar";
+import AboutMe from "../components/AboutMe";
+import ContactWays from "../components/ContactWays";
+
+export default function Home() {
+  return (
+    <>
+      <Avatar />
+      <AboutMe />
+      <ContactWays />
+    </>
+  );
+}
